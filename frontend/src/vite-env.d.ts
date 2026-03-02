695cf84e5988897c96e54b0145104d7aa4bbec6f
hiyasame <coldrain608@gmail.com>
2026-03-02 19:58:46 +0800

    bugfix: 修复直播推流问题

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,7 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string
   readonly VITE_WS_BASE_URL: string
-  readonly VITE_SRS_BASE_URL: string
+  readonly VITE_SRS_HTTP_BASE_URL: string
+  readonly VITE_SRS_RTMP_BASE_URL: string
   readonly VITE_CDN_BASE_URL: string
 }
 
